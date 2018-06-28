b682247d519c11d8985f9ef44ac86b2f7727251e
Erlimar Silva Campos <erlimar@gmail.com>
2018-06-28 09:55:40 -0300

    Atualiza arquivos de projeto

--- scope_begin.txt.js
+++ scope_begin.txt.js
@@ -1,48 +1,48 @@
 
-(function($, $elm, $app, $require, $namespace) {
+(function ($, $elm, $app, $require, $namespace) {
 
-var _APP_NAMESPACE_KEY_ = '_app_namespace_';
+    var _APP_NAMESPACE_KEY_ = '_app_namespace_';
 
-// Inicializa namespace exclusivo para aplicação do usuário
-var _APP_ = window[__TOAD__][_APP_NAMESPACE_KEY_] = {};
+    // Inicializa namespace exclusivo para aplicação do usuário
+    var _APP_ = window[__TOAD__][_APP_NAMESPACE_KEY_] = {};
 
-$app.$jq = $;
+    $app.$jq = $;
 
-/**
- * @code
-$app.namespace('utils', function(exports) {
-    var myData = {}
-    var myFunction = function(){}
-
-    exports.data = myData
-    exports.func = myFunction
-})
- */
-$app.namespace = function (_, __) {
-    __((_APP_[_] = _APP_[_] || {}));
-}
-
-/**
- * @code
-var utils = app.require('utils')
-
-utils.func(utils.data)
- */
-$app.require = function (_) {
-    var require = {};
-
-    // Objetos globais exceto o namespace da aplicação
-    for (var k in window[__TOAD__][_]) {
-        // TODO: Ignorar $jq, namespace, require também
-        if (k === _APP_NAMESPACE_KEY_)
-            continue;
-        require[k] = window[__TOAD__][_][k];
+    /**
+     * @code
+    $app.namespace('utils', function(exports) {
+        var myData = {}
+        var myFunction = function(){}
+    
+        exports.data = myData
+        exports.func = myFunction
+    })
+     */
+    $app.namespace = function (_, __) {
+        __((_APP_[_] = _APP_[_] || {}));
     }
 
-    // Objetos da aplicação
-    for (var k in _APP_[_]) {
-        require[k] = _APP_[_][k];
-    }
+    /**
+     * @code
+    var utils = app.require('utils')
+    
+    utils.func(utils.data)
+     */
+    $app.require = function (_) {
+        var require = {};
 
-    return require;
-}
+        // Objetos globais exceto o namespace da aplicação
+        for (var k in window[__TOAD__][_]) {
+            // TODO: Ignorar $jq, namespace, require também
+            if (k === _APP_NAMESPACE_KEY_)
+                continue;
+            require[k] = window[__TOAD__][_][k];
+        }
+
+        // Objetos da aplicação
+        for (var k in _APP_[_]) {
+            require[k] = _APP_[_][k];
+        }
+
+        return require;
+    }
