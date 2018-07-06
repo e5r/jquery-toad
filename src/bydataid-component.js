10df7c0c37010ee097e94a7c20d97e6f54bb6000
Erlimar Silva Campos <erlimar@gmail.com>
2018-07-06 14:06:44 -0300

    Remove Plugin jQuery byDataId

--- src/bydataid-component.js
+++
@@ -1,9 +0,0 @@
-$namespace(2, 'core', function (exports) {
-    var BIND_ELEMENT_DATA_CTRL = '$ctrl';
-
-    exports.ByDataIdComponent = $.fn['byDataId'] = function ByDataIdComponent(dataId) {
-        var selector = '[data-id="{id}"]';
-
-        return $(selector.replace('{id}', dataId), $(this));
-    }
-})
